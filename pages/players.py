d834169fe22e53b7eeec3c0cc36e578b62bc4109
Psyrax <psyrax@opiumgarden.org>
2025-02-14 22:23:26 -0600

    added event data

--- pages/players.py
+++ pages/players.py
@@ -4,13 +4,9 @@ import plotly.express as px
 
 st.set_page_config(
     layout="wide",
-    page_title="Pokemon TCG Stats del tio Pay",
+    page_title="Jugadores - Pokemon TCG Stats del tio Pay",
 )
 
-st.title("Pokemon TCG Stats del tio Pay")
-st.text("Hola, soy el tio Pay (psyrax) y aqui puedes ver mis estadisticas de torneos de Pokemon TCG")
-st.text("Los datos son extraidos de la pagina oficial de Pokemon TCG y se actualizan automaticamente por medio de un script de python el cual puedes encontrar en el repositorio del proyecto.")
-st.text("El script de python extrae los datos de los torneos de Pokemon TCG en los que he participado y los guarda en un archivo csv, el cual es leido por esta aplicacion web. Si no apareces en este listado, es porque no has participado en torneos de Pokemon TCG donde he estado yo.")
 
 df = pd.read_csv('./data/all.csv')
 df['date'] = pd.to_datetime(df['date'])
--- parseResults.py
+++ parseResults.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("pages/players.py"), st.Page("pages/venues.py")])
+st.set_page_config(
+    layout="wide",
+    page_title="Pokemon TCG Stats del tio Pay",
+)
+
+st.title("Pokemon TCG Stats del tio Pay")
+st.text("Hola, soy el tio Pay (psyrax) y aqui puedes ver mis estadisticas de torneos de Pokemon TCG")
+st.text("Los datos son extraidos de la pagina oficial de Pokemon TCG y se actualizan automaticamente por medio de un script de python el cual puedes encontrar en el repositorio del proyecto.")
+st.text("El script de python extrae los datos de los torneos de Pokemon TCG en los que he participado y los guarda en un archivo csv, el cual es leido por esta aplicacion web. Si no apareces en este listado, es porque no has participado en torneos de Pokemon TCG donde he estado yo.")
+
+pg = st.navigation([st.Page("pages/players.py", "Jugaores"), st.Page("pages/venues.py", "Venues")])
 pg.run()
