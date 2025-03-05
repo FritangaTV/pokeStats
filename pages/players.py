79761a04ec7572f5980246f913cb20e5b3caa97e
Psyrax <psyrax@opiumgarden.org>
2025-03-05 13:26:47 -0600

    updated data and layout

--- pages/players.py
+++ pages/players.py
@@ -6,71 +6,97 @@ import plotly.express as px
 
 df = pd.read_csv('./data/standings/all.csv', low_memory=False)
 df['date'] = pd.to_datetime(df['date'])
-st.title("Filtros")
-st.text("Puedes seleccionar los jugadores y eventos que quieras ver en el grafico de abajo. Por favor se amable y no me critiques por mi desempeño en los torneos, soy un tio muy sensible.")
-playerSelect = st.multiselect( "Jugadores", df['Name'].unique(), ["psyrax", "Oscar Vera"])
-eventSelect = st.multiselect( "Eventos", df['event_type'].unique(), df['event_type'].unique())
+
+filterCol1, filterCol2, filterCol3 = st.columns(3)
+
+with filterCol1:
+    st.title("Filtros")
+    st.text("Puedes seleccionar los jugadores y eventos que quieras ver en el grafico de abajo. Por favor se amable y no me critiques por mi desempeño en los torneos, soy un tio muy sensible.")
+with filterCol2:
+    playerSelect = st.multiselect( "Jugadores", df['Name'].unique(), ["psyrax", "Oscar Vera"])
+with filterCol3:
+    eventSelect = st.multiselect( "Eventos", df['event_type'].unique(), df['event_type'].unique())
 
 
 selectedDF = df.copy()
 
-if len(playerSelect) > 0:
-    mask = selectedDF['Name'].isin(playerSelect)
-    selectedDF = selectedDF[mask]
-    
-if len(eventSelect) > 0:
-    eventMask = selectedDF['event_type'].isin(eventSelect)
-    selectedDF = selectedDF[eventMask]
-    
-selectedDF['date'] = pd.to_datetime(selectedDF['date'])
-selectedDF['Reverse Rank'] = 100-selectedDF['rank']
-selectedDF = selectedDF.sort_values(by=['date'])
+
+resultsTab, dataTab = st.tabs(["Resultados", "Data"])
 
 
-playerScores = selectedDF.groupby('Name').agg({'rank': ['mean', 'std', 'sum']}).reset_index()
-playerScores['Eventos'] = selectedDF.groupby('Name').size().values
-playerScores['Media inversa'] = 100-playerScores[('rank', 'mean')].round(2)
-playerScores['Score'] = (100-playerScores[('rank', 'mean')].round(2))*playerScores['Eventos']
-playerScores = playerScores.sort_values(by=[('rank', 'mean')], ascending=True).reset_index(drop=True)
-st.title("Estadisticas")
-st.text("El Score se calcula como la media inversa de la posicion en los eventos multiplicada por la cantidad de eventos en los que ha participado el jugador.")
-st.dataframe(playerScores, use_container_width=True)
+with resultsTab:
+    resultsCol1, resultsCol2 = st.columns(2)
+    if len(playerSelect) > 0:
+        mask = selectedDF['Name'].isin(playerSelect)
+        selectedDF = selectedDF[mask]
+        
+    if len(eventSelect) > 0:
+        eventMask = selectedDF['event_type'].isin(eventSelect)
+        selectedDF = selectedDF[eventMask]
+        
+        selectedDF['date'] = pd.to_datetime(selectedDF['date'])
+        selectedDF['Reverse Rank'] = 100-selectedDF['rank']
+        selectedDF = selectedDF.sort_values(by=['date'])
 
-st.title("Gráficos de desempeño")
-st.subheader("Posición por evento")
-st.text("Aqui puedes ver el grafico de la data filtrada acomodada por percentiles. Mas alto es mejor.")
-st.text("Un percentil es una medida estadística que indica el valor por debajo del cual se encuentra un porcentaje dado de observaciones en un grupo de observaciones. Es decir, que tan bien o mal le fue a un jugador en un torneo en comparacion con los demas jugadores sin que importe mucho la cantidad de participantes.")
-fig = px.line(selectedDF, x="date", y="Reverse Rank", title='Percentil Inverso de posición por evento', color="Name")
-st.plotly_chart(fig)
 
-st.title("Data filtrada")
-st.text("Aqui puedes ver la data filtrada por los jugadores y eventos seleccionados")
-st.dataframe(selectedDF, use_container_width=True)
+        playerScores = selectedDF.groupby('Name').agg({'rank': ['mean', 'std', 'sum']}).reset_index()
+        playerScores['Eventos'] = selectedDF.groupby('Name').size().values
+        playerScores['Media inversa'] = 100-playerScores[('rank', 'mean')].round(2)
+        playerScores['Score'] = (100-playerScores[('rank', 'mean')].round(2))*playerScores['Eventos']
+        playerScores = playerScores.sort_values(by=[('rank', 'mean')], ascending=True).reset_index(drop=True)
+
+        with resultsCol1:
+            st.title("Estadisticas")
+            st.text("El Score se calcula como la media inversa de la posicion en los eventos multiplicada por la cantidad de eventos en los que ha participado el jugador.")
+            st.dataframe(playerScores, use_container_width=True)
+            st.write("TODO: rank por tipo de evento por jugador, distribucion de posiciones por evento")
+            
+        with resultsCol2:
+            st.title("Gráficos de desempeño")
+            st.subheader("Posición por evento")
+            st.text("Aqui puedes ver el grafico de la data filtrada acomodada por percentiles. Mas alto es mejor.")
+            st.text("Un percentil es una medida estadística que indica el valor por debajo del cual se encuentra un porcentaje dado de observaciones en un grupo de observaciones. Es decir, que tan bien o mal le fue a un jugador en un torneo en comparacion con los demas jugadores sin que importe mucho la cantidad de participantes.")
+            fig = px.line(selectedDF, x="date", y="Reverse Rank", title='Percentil Inverso de posición por evento', color="Name")
+            st.plotly_chart(fig)
+
+with dataTab:
+    st.title("Data filtrada")
+    st.text("Aqui puedes ver la data filtrada por los jugadores y eventos seleccionados")
+    st.dataframe(selectedDF, use_container_width=True)
+
+st.divider()
 
 st.title("Data completa")
 st.text("Aqui puedes ver la data completa")
-st.dataframe(df, use_container_width=True)
+leaderTab, scoreTab, fullDataTab = st.tabs(["Leaderboard", "Score", "Data completa"])
 
-eventDistBox = px.box(df, x="event_name", y="rank", color="event_type", title='Distribución de posiciones por evento', height=600)
-eventDistBox.update_layout(xaxis=dict(
-    type="category",
-    categoryorder='array', 
-    categoryarray=df.sort_values(by=["date"]).reset_index()['event_name'].unique()
-))
+with leaderTab:
+    st.title("Leaderboard")
+    st.subheader("Número de eventos")
+    eventDF = df.copy() 
+    eventDF['Eventos'] = eventDF.groupby('Name')['Name'].transform('count')
+    st.dataframe(eventDF.groupby('Name').agg({'Eventos': 'max'}).sort_values(by='Eventos', ascending=False).reset_index(), use_container_width=True)
+    
+with scoreTab:
+    st.subheader("Score")
+    st.text("El Score se calcula como la media inversa de la posicion en los eventos multiplicada por la cantidad de eventos en los que ha participado el jugador.")
+    leaderScores = df.groupby('Name').agg({'rank': ['mean', 'std', 'sum']}).reset_index()
+    leaderScores['Eventos'] = df.groupby('Name').size().values
+    leaderScores['Media inversa'] = 100-leaderScores[('rank', 'mean')].round(2)
+    leaderScores['Score'] = (100-leaderScores[('rank', 'mean')].round(2))*leaderScores['Eventos']
+    leaderScores = leaderScores.sort_values(by=["Score"], ascending=False).reset_index(drop=True)
+    st.dataframe(leaderScores, use_container_width=True)
+with fullDataTab:
+    st.dataframe(df, use_container_width=True)
 
-st.plotly_chart(eventDistBox)
+    eventDistBox = px.box(df, x="event_name", y="rank", color="event_type", title='Distribución de posiciones por evento', height=600)
+    eventDistBox.update_layout(xaxis=dict(
+        type="category",
+        categoryorder='array', 
+        categoryarray=df.sort_values(by=["date"]).reset_index()['event_name'].unique()
+    ))
 
-st.title("Leaderboard")
-st.subheader("Número de eventos")
-eventDF = df.copy() 
-eventDF['Eventos'] = eventDF.groupby('Name')['Name'].transform('count')
-st.dataframe(eventDF.groupby('Name').agg({'Eventos': 'max'}).sort_values(by='Eventos', ascending=False).reset_index(), use_container_width=True)
+    st.plotly_chart(eventDistBox)
 
-st.subheader("Score")
-st.text("El Score se calcula como la media inversa de la posicion en los eventos multiplicada por la cantidad de eventos en los que ha participado el jugador.")
-leaderScores = df.groupby('Name').agg({'rank': ['mean', 'std', 'sum']}).reset_index()
-leaderScores['Eventos'] = df.groupby('Name').size().values
-leaderScores['Media inversa'] = 100-leaderScores[('rank', 'mean')].round(2)
-leaderScores['Score'] = (100-leaderScores[('rank', 'mean')].round(2))*leaderScores['Eventos']
-leaderScores = leaderScores.sort_values(by=["Score"], ascending=False).reset_index(drop=True)
-st.dataframe(leaderScores, use_container_width=True)
+
+
--- pages/venues.py
+++ pages/venues.py
@@ -6,12 +6,16 @@ import plotly.express as px
 
 st.header("Stats de lugares")
 
+filterCol1, filterCol2 = st.columns(2)
 venuesDF = pd.read_csv('./data/events/all_events.csv')
 venuesDF['Date'] = pd.to_datetime(venuesDF['Date'])
 venuesDF = venuesDF.sort_values(by=['Date']).reset_index(drop=True)
+with filterCol1:
+    selectedEvents = venuesDF[venuesDF['Event Type']!= 'Regional Championships']['Event Type'].unique()
+    eventTypeSelect = st.multiselect( "Tipo de evento",venuesDF['Event Type'].unique(), selectedEvents)
 
-eventTypeSelect = st.multiselect( "Tipo de evento",venuesDF['Event Type'].unique(),venuesDF['Event Type'].unique())
-venueSelect = st.multiselect( "Lugares",venuesDF['Venue Name'].unique(),venuesDF['Venue Name'].unique())
+with filterCol2:
+    venueSelect = st.multiselect( "Lugares",venuesDF['Venue Name'].unique(),venuesDF['Venue Name'].unique())
 
 
 selectedVenueDF = venuesDF.copy()
@@ -24,10 +28,15 @@ if len(eventTypeSelect) > 0:
     eventMask = selectedVenueDF['Event Type'].isin(eventTypeSelect)
     selectedVenueDF = selectedVenueDF[eventMask]
 
-st.dataframe(selectedVenueDF, use_container_width=True)
 
-priceFig = px.scatter(selectedVenueDF, x="Date", y="Masters Division", title='Jugadores por evento', color="Event Type", size="Total Players", height=600,  trendline="ols")
-st.plotly_chart(priceFig)
+graphTab, dataTab = st.tabs(["Graficos", "Data"])
 
+with graphTab:
+    priceFig = px.scatter(selectedVenueDF, x="Date", y="Masters Division", title='Jugadores por evento', color="Event Type", size="Total Players", height=600,  trendline="ols")
+    st.plotly_chart(priceFig)
 
+with dataTab:
+    st.dataframe(selectedVenueDF, use_container_width=True)
+
+st.divider()
 st.dataframe(venuesDF, use_container_width=True)
